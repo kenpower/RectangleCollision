9d8de4880772a8b4715cc3ac167fa300426f9681
Ken Power <ken@kenpower.com>
2021-10-18 12:17:00 +0100

    suite of tests

--- RectangleCollision/Rectangle.h
+++ RectangleCollision/Rectangle.h
@@ -3,7 +3,23 @@ class Rectangle {
 	double x, y;
 	double width, height;
 
+	double right() const { return x + width / 2; }
+	double left() const { return x - width / 2; }
+	double top() const { return y + height / 2; }
+	double bottom() const { return y - height / 2; }
+
 public:
 	Rectangle(double x, double y, double width, double height):
 		x(x), y(y), width(width), height(height) {}
+
+
+	bool collidesWith(const Rectangle& other) {
+		if (right() < other.left() || left() > other.right())
+			return false;
+
+		if (top() < other.bottom() || bottom() > other.top())
+			return false;
+
+		return true;
+	}
 };
--- RectangleTest/test.cpp
+++ RectangleTest/test.cpp
@@ -1,6 +1,69 @@
 #include "pch.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+#include "../RectangleCollision/Rectangle.h"
+
+TEST(CollidesWithSame, Rectangle) {
+
+	Rectangle a{ 0,0,1,1 };
+	Rectangle b{ 0,0,1,1 };
+
+	ASSERT_TRUE(a.collidesWith(b));
+}
+
+TEST(DoesNotCollidesOnLeft, Rectangle) {
+
+	Rectangle a{ 0,0,1,1 };
+	Rectangle b{ 2,0,1,1 };
+
+	ASSERT_FALSE(a.collidesWith(b));
+}
+
+TEST(DoesNotCollidesOnRight, Rectangle) {
+
+	Rectangle a{ 0,0,1,1 };
+	Rectangle b{ 2,0,1,1 };
+
+	ASSERT_FALSE(b.collidesWith(a));
+}
+
+TEST(DoesNotCollidesAbove, Rectangle) {
+
+	Rectangle a{ 0,0,1,1 };
+	Rectangle b{ 0,2,1,1 };
+
+	ASSERT_FALSE(a.collidesWith(b));
+}
+
+TEST(DoesNotCollidesUnder, Rectangle) {
+
+	Rectangle a{ 0,0,1,1 };
+	Rectangle b{ 0,-2,1,1 };
+
+	ASSERT_FALSE(a.collidesWith(b));
+}
+
+TEST(DoesNotCollidesDiagonal, Rectangle) {
+
+	Rectangle a{ 0,0,1,1 };
+	Rectangle b{ 2,2,1,1 };
+
+	ASSERT_FALSE(a.collidesWith(b));
+}
+
+TEST(DoesCollidesDiagonalOverlap, Rectangle) {
+
+	Rectangle a{ 0,0,2,2 };
+	Rectangle b{ 2,2,2,2 };
+
+	ASSERT_TRUE(a.collidesWith(b));
+	ASSERT_TRUE(b.collidesWith(a));
+}
+
+TEST(DoesCollidesDiagonalInside, Rectangle) {
+
+	Rectangle a{ 0,0,3,3 };
+	Rectangle b{ 1,1,1,1 };
+
+	ASSERT_TRUE(a.collidesWith(b));
+	ASSERT_TRUE(b.collidesWith(a));
 }
