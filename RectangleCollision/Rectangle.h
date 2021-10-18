9aaf1959a9f7c2add020606256a058e598ba174c
powerk <powerk@A307J001>
2021-10-18 11:00:06 +0100

    Add project files.

---
+++ RectangleCollision/Rectangle.h
@@ -0,0 +1,9 @@
+#pragma once
+class Rectangle {
+	double x, y;
+	double width, height;
+
+public:
+	Rectangle(double x, double y, double width, double height):
+		x(x), y(y), width(width), height(height) {}
+};
